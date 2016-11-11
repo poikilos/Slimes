82b810a8c0a7ef04a806fdbe087252e695baf823
jordan4ibanez <jordan4ibanez@gmail.com>
2016-11-10 19:28:02 -0500

    Drops and stuff

--- init.lua
+++ init.lua
@@ -27,13 +27,8 @@ minetest.register_entity("slimes:slime_big", {
 		slimes.state_change(self,dtime)
 		slimes.movement(self)
 	end,
-
-	--add in smaller slmies on kill
 	on_punch = function(self)
-		local hp = self.object:get_hp()
-		if hp <= 0 then
-			slimes.slime_duplicate(self)
-		end
+		slimes.slime_punch(self)
 	end,
 })
 
@@ -64,15 +59,8 @@ minetest.register_entity("slimes:slime_medium", {
 		slimes.state_change(self,dtime)
 		slimes.movement(self)
 	end,
-	--add in smaller slmies on kill
 	on_punch = function(self)
-		local pos = self.object:getpos()
-		local hp = self.object:get_hp()
-		if hp <= 0 then
-			for i = 1,math.random(6,16) do
-				minetest.add_entity(pos, "slimes:slime_small")
-			end
-		end
+		slimes.slime_punch(self)
 	end,
 })
 --small slimes
@@ -102,7 +90,98 @@ minetest.register_entity("slimes:slime_small", {
 		slimes.state_change(self,dtime)
 		slimes.movement(self)
 	end,
+	on_punch = function(self)
+		slimes.slime_punch(self)
+	end,
 })
+
+local load_technic_items = false
+for _,v in pairs(minetest.get_modnames()) do 
+	if v == "technic" then
+		load_technic_items = true
+		break
+	end
+end
+
+if load_technic_items == true then
+	slimes.slime_drop = {"slimes:beer","slimes:hawaiin_pizza","mesecons_materials:glue"}
+	minetest.register_craftitem("slimes:beer", {
+		description = "Ice Cold Beer",
+		inventory_image = "beer.png",
+		on_use = minetest.item_eat(20),
+	})
+	minetest.register_craftitem("slimes:hawaiin_pizza", {
+		description = "Delicious Hawaiin Pizza",
+		inventory_image = "hawaiin_pizza.png",
+		on_use = minetest.item_eat(20),
+	})	
+else
+	slimes.slime_drop = {"default:sapling", "default:dirt_with_grass"}
+end
+
+--when a slime is punched
+slimes.slime_punch = function(self)
+	local hp = self.object:get_hp()
+	local size_check = 0
+	local pos = self.object:getpos()
+	if self.slime_size == "big" then
+		size_check = 3
+	elseif self.slime_size == "medium" then
+		size_check = 0.5
+	elseif self.slime_size == "small" then
+		size_check = 0.1
+	end
+	minetest.sound_play("slime_hit", {
+		max_hear_distance = 20,
+		gain = 10.0,
+		object = self.object,
+	})
+	--die
+	if hp <= 0 then
+		minetest.add_particlespawner({
+			amount = 80,
+			time = 0.1,
+			minpos = {x=pos.x-(size_check), y=pos.y-(size_check), z=pos.z-(size_check)},
+			maxpos = {x=pos.x+(size_check), y=pos.y+(size_check), z=pos.z+(size_check)},
+			minvel = {x=-5, y=-5, z=-5},
+			maxvel = {x=5, y=5, z=5},
+			minacc = {x=0, y=-10, z=0},
+			maxacc = {x=0, y=-10, z=0},
+			minexptime = 1,
+			maxexptime = 1,
+			minsize = size_check*2,
+			maxsize = size_check,
+			collisiondetection = true,
+			vertical = false,
+			texture = "slime_particles.png",
+		})
+		slimes.slime_duplicate(self)
+		if self.slime_size == "small" then
+			minetest.add_item(pos,slimes.slime_drop[math.random(1,table.getn(slimes.slime_drop))])
+		end
+	--hurt
+	else
+		minetest.add_particlespawner({
+			amount = 40,
+			time = 0.1,
+			minpos = {x=pos.x-(size_check), y=pos.y-(size_check), z=pos.z-(size_check)},
+			maxpos = {x=pos.x+(size_check), y=pos.y+(size_check), z=pos.z+(size_check)},
+			minvel = {x=-2, y=-2, z=-2},
+			maxvel = {x=2, y=2, z=2},
+			minacc = {x=0, y=-10, z=0},
+			maxacc = {x=0, y=-10, z=0},
+			minexptime = 1,
+			maxexptime = 1,
+			minsize = size_check*2,
+			maxsize = size_check,
+			collisiondetection = true,
+			vertical = false,
+			texture = "slime_particles.png",
+		})
+	end
+	
+end
+
 
 --the state timer for slimes
 slimes.state_change = function(self,dtime)
@@ -120,7 +199,7 @@ slimes.state_change = function(self,dtime)
 	end
 end
 
---how slimes move around
+--how slimes check to move around
 slimes.movement = function(self)
 	local pos = self.object:getpos()
 	local vel = self.object:getvelocity()
@@ -140,7 +219,7 @@ slimes.movement = function(self)
 			self.object:setvelocity({x=0,y=vel.y,z=0})
 		end
 	end
-	--walking around
+	--jumping around
 	if self.state == 1 and vel.y <= 0.5 then
 		if vel.y == 0 then
 			--check for node below before jumping
@@ -150,6 +229,8 @@ slimes.movement = function(self)
 		end
 	end
 end
+
+--how a slime moves around
 slimes.jump = function(self,pos)
 	local y_check = 0
 	local jump_height = 0
@@ -191,6 +272,11 @@ slimes.jump = function(self,pos)
 	minetest.after(1,function(self)
 		self.object:set_animation({x=10,y=50},7, 0)
 	end,self)
+	minetest.sound_play("slime_jump", {
+		max_hear_distance = 6,
+		gain = 1.0,
+		object = self.object,
+	})
 end
 
 --add smaller slimes on die
